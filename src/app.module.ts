ffdf1ebc0ef8b9afe17dccaa237d01cacf4273f7
Cedric <ramanantsoac1@gmail.com>
2023-09-20 12:38:03 +0300

    ✨ : first resources for the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BookmarkModule, UserModule, AuthModule],
 })
 export class AppModule {}
